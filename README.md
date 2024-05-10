56e871166e07b2e35d4ee22bc1e01fda3f18ad63
camillehe1992 <camille.he@outlook.com>
2024-05-10 21:08:12 +0800

    chore: install element-plus

--- README.md
+++ README.md
@@ -7,8 +7,16 @@ A repository that is used to to a Python web application using Flask and Vue.
 Frontend: Vite 5, Vue3 , Element UI.
 Backend: Python Flask.
 
+## Get Started
+
+```bash
+cd vue-app
+npm install
+npm run dev
+```
 
 ## References
 
 https://vuejs.org/guide/introduction.html
 https://vitejs.dev/guide/
+https://element-plus.org/en-US/
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus, { size: "small", zIndex: 3000 }).mount("#app");
